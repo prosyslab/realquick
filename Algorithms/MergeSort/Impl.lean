e05236dbb6f583ea85b21c921b7b36b6edfe9ea2
Bongjun <bongjun.jang@kaist.ac.kr>
2026-09-12 12:11:40 +0000

    [MergeSort] implement mergeSort with termination proof

--- Algorithms/MergeSort/Impl.lean
+++ Algorithms/MergeSort/Impl.lean
@@ -43,4 +43,53 @@ theorem splitAlt_balanced (xs : List Int) :
   have h := splitAlt_balanced_oriented xs b
   cases b <;> simp at h <;> omega
 
+theorem splitAlt_preserves_length (b : Bool) (xs : List Int) :
+  let (s1, s2) := splitAlt b xs
+  s1.length + s2.length = xs.length := by
+  induction xs generalizing b with
+  | nil =>
+    simp [splitAlt]
+  | cons x xs ih =>
+    cases b with
+    | false =>
+      simp [splitAlt]
+      have h := ih true
+      omega
+    | true =>
+      simp [splitAlt]
+      have h := ih false
+      omega
+
+private theorem splitAlt_lt_length_of_length_ge_two
+    (b : Bool) (xs : List Int) (hxs : 2 ≤ xs.length) :
+    (splitAlt b xs).1.length < xs.length ∧
+      (splitAlt b xs).2.length < xs.length := by
+  have hsum := splitAlt_preserves_length b xs
+  have hbalanced := splitAlt_balanced xs b
+  dsimp at hbalanced
+  omega
+
+def merge : List Int → List Int → List Int
+  | [], ys => ys
+  | xs, [] => xs
+  | x :: xs, y :: ys =>
+    if x ≤ y then x :: merge xs (y :: ys)
+    else y :: merge (x :: xs) ys
+
+def mergeSort : List Int → List Int
+  | [] => []
+  | [x] => [x]
+  | x :: y :: xs =>
+    match h : splitAlt true (x :: y :: xs) with
+    | (a, b) =>
+      let a' := mergeSort a
+      let b' := mergeSort b
+      merge a' b'
+termination_by xs => xs.length
+decreasing_by
+  all_goals
+    have hlt := splitAlt_lt_length_of_length_ge_two true (x :: y ::xs) (by simp)
+    rw [h] at hlt
+    first | exact hlt.1 | exact hlt.2
+
 end Algorithms.MergeSort.Impl
