e260ab72f64f46e73fd0bcff9a9850b004c057b5
Bongjun <bongjun.jang@kaist.ac.kr>
2026-09-12 12:38:34 +0000

    [MergeSort] simplify correctness prop

--- Algorithms/MergeSort/Correctness.lean
+++ Algorithms/MergeSort/Correctness.lean
@@ -6,7 +6,7 @@ namespace Algorithms.MergeSort.Correctness
 
 def Sorted (xs : List Int) : Prop := xs.Pairwise (fun x y => x ≤ y)
 
-def Correct (impl : List Int → TimeM (List Int)) : Prop :=
-  ∀ xs, Sorted (impl xs).value ∧ (impl xs).value.Perm xs
+def Correct (impl : List Int → List Int) : Prop :=
+  ∀ xs, Sorted (impl xs) ∧ (impl xs).Perm xs
 
 end Algorithms.MergeSort.Correctness
